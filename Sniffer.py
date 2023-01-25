6e0518901840aa395bce6df5a74abac10357dde1
irfan378 <irfandominate@gmail.com>
2023-01-25 08:59:22 +0530

    ICMP decode

--- Sniffer.py
+++ Sniffer.py
@@ -4,7 +4,17 @@ import struct
 import ipaddress
 import sys
 # host to listen on
-HOST = '192.168.247.249'
+HOST = '192.168.42.203'
+
+
+class ICMP:
+    def __init__(self, buff):
+        header = struct.unpack('<BBHHH', buff)
+        self.type = header[0]
+        self.code = header[1]
+        self.sum = header[2]
+        self.id = header[3]
+        self.seq = header[4]
 
 
 class IP:
@@ -12,25 +22,26 @@ class IP:
         header = struct.unpack('<BBHHHBBH4s4s', buff)
         self.ver = header[0] >> 4
         self.ihl = header[0] & 0xF
-        self.tos=header[1]
-        self.len=header[2]
-        self.id=header[3]
-        self.offset=header[4]
-        self.ttl=header[5]
-        self.protocol_num=header[6]
-        self.sum=header[7]
-        self.src=header[8]
-        self.dst=header[9]
+        self.tos = header[1]
+        self.len = header[2]
+        self.id = header[3]
+        self.offset = header[4]
+        self.ttl = header[5]
+        self.protocol_num = header[6]
+        self.sum = header[7]
+        self.src = header[8]
+        self.dst = header[9]
         # human readable IP address
-        self.src_address=ipaddress.ip_address(self.src)
-        self.dst_address=ipaddress.ip_address(self.dst)
-        #map protocol constants to their names
-        self.protocol_map={1:"ICMP",6:"TCP",17:"UDP"}
+        self.src_address = ipaddress.ip_address(self.src)
+        self.dst_address = ipaddress.ip_address(self.dst)
+        # map protocol constants to their names
+        self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
         try:
-            self.protocol=self.protocol_map[self.protocol_num]
+            self.protocol = self.protocol_map[self.protocol_num]
         except Exception as e:
             print('%s No protocol for %s' % (e, self.protocol_num))
-        self.protocol=str(self.protocol_num)
+        self.protocol = str(self.protocol)
+
 
 def sniff(host):
     # create raw socket, bin to public interface
@@ -44,23 +55,35 @@ def sniff(host):
     sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
     if os.name == 'nt':
         sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-    
+
     try:
         while True:
             # read a packet
-            raw_buffer=sniffer.recvfrom(65535)[0]
+            raw_buffer = sniffer.recvfrom(65535)[0]
             # create an IP header from the first 20 bytes
             ip_header = IP(raw_buffer[0:20])
-            # print the detected protocol and hosts
-            print('Protocol: %s %s -> %s' % (ip_header.protocol,ip_header.src_address,ip_header.dst_address))
+            if ip_header.protocol == "ICMP":
+                # print the detected protocol and hosts
+                print('Protocol: %s %s -> %s' % (ip_header.protocol,
+                      ip_header.src_address, ip_header.dst_address))
+                print(f'Version: {ip_header.ver}')
+                print(f'Header Length: {ip_header.ihl} TTL: {ip_header.ttl}')
+                # # calculate where our ICMP packet starts
+                offset = ip_header.ihl * 4
+                buf = raw_buffer[offset:offset + 8]
+                icmp_header = ICMP(buf)
+                print('ICMP -> Type: %s Code: %s\n' %
+                      (icmp_header.type, icmp_header.code))
     except KeyboardInterrupt:
         # if we're on Windows, turn off promiscuous mode
         if os.name == 'nt':
             sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
         sys.exit()
+
+
 if __name__ == '__main__':
-    if len(sys.argv)==2:
-        host=sys.argv[1]
+    if len(sys.argv) == 2:
+        host = sys.argv[1]
     else:
-        host='192.168.42.102'
+        host = '192.168.42.203'
     sniff(host)
