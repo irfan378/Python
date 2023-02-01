0f632cc54cdf45b873126908ee2bb9a22ea0e304
irfan378 <irfandominate@gmail.com>
2023-02-01 08:33:19 +0530

    Detector.py

--- Detector.py
+++ Detector.py
@@ -1,8 +1,8 @@
 import cv2
 import os
-ROOT = '/root/Desktop/pictures'
-FACES = '/root/Desktop/faces'
-TRAIN = '/root/Desktop/training'
+ROOT = '/home/irfan/Downloads'
+FACES = '/home/irfan/Desktop'
+TRAIN = '/home/irfan/Public/Python'
 def detect(srcdir=ROOT,tgtdir=FACES,train_dir=TRAIN):
     #Iterate over the JPG files in the source directory
     for fname in os.listdir(srcdir):
@@ -14,5 +14,21 @@ def detect(srcdir=ROOT,tgtdir=FACES,train_dir=TRAIN):
         img = cv2.imread(fullname)
         if img is None:
             continue
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        training=os.path.join(train_dir,'haarcascade_frontalface_alt.xml')
+        cascade=cv2.CascadeClassifier(training)
+        rects=cascade.detectMultiScale(gray,1.3,5)
+        try:
+            if rects.any():
+                print('Got a face')
+                #convert the returned rects data to actual coordinates: (x1, y1, x1+width,y1+height) or (x1, y1, x2, y2).
+                rects[:, 2:] += rects[:, :2]
+        except AttributeError:
+            print(f'No faces found in {fname}.')
+            continue
+        # highlight the faces in the image
+        for x1, y1, x2, y2 in rects:
+            cv2.rectangle(img, (x1, y1), (x2, y2), (127, 255, 0), 2)
+        cv2.imwrite(newname, img)
 if __name__ =='__main__':
     detect()
