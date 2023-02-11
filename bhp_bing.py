6749a691ac2159eadd68c3d27bf0b109866870f0
irfan378 <irfandominate@gmail.com>
2023-02-11 09:40:34 +0530

    bhp_bing

---
+++ bhp_bing.py
@@ -0,0 +1,11 @@
+from burp import IBurpExtender
+from burp import IContextMenuFactory
+from java.net import URL
+from java.util import ArrayList
+from javax.swing import JMenuItem
+from thread import start_new_thread
+import json
+import socket
+import urllib
+API_KEY = "YOURKEY"
+API_HOST = 'api.cognitive.microsoft.com'
